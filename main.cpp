a310c1885a324d7273e1d59ebc03d4ba60e5a8a7
s30299 <s30299@pjwstk.edu.pl>
2023-12-18 06:45:16 -0500

    added division

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main(){
 	std::cout << "odejmowanie";
 	std::cout << a-10 << "\n";
 	std::cout << "mnozenie: :";
-	std::cout << a*10;
+	std::cout << a*10 << "\n";
+	std::cout << "dzielenie: ";
+	std::cout << a/10 << "\n";
 	return 0;
 }
